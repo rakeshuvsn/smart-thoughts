57ae947feeb82fb86d9bffab5a3d189b9d8cfa4c
rakesh <rakeshuvsn@users.noreply.github.com>
2017-08-01 13:23:09 -0400

    initial yeoman angular generator setup is done

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name smartThoughtsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the smartThoughtsApp
+ */
+angular.module('smartThoughtsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
